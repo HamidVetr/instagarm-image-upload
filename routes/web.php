0635b702b6ea676f09975558e3b91def1e72add7
Hamid <Hamid.vetr@gmail.com>
2017-05-30 12:56:46 +0430

    added photo upload using dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ Route::get('/', function () {
 });
 
 Route::get('instagram', 'InstagramController@index');
+
+Route::get('instalogin', function (){
+    $ig = new \InstagramAPI\Instagram();
+
+    $username = 'horoofnegar';
+    $password = 'Serint9263';
+
+    $ig->setUser($username, $password);
+    $ig->login();
+
+    $metadata = [
+        'caption' => 'تست آپلود عکس از داشبورد توی صفحه اینستا گرام',
+//        'location' => $location, // $location must be an instance of Location class
+    ];
+
+    $photoFile = "C:\\7.jpg";
+
+// if you want only a caption, you can simply do this:
+//    $metadata = ['caption' => 'My awesome caption'];
+
+    $ig->uploadTimelinePhoto($photoFile, $metadata);
+
+    dd($ig);
+});
